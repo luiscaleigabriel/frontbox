e146a776946aa1213d5c7bbc49df59ffb9ec6c39
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2023-12-29 10:37:44 +0100

    menu mobile

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function actionMenu() {
+
+    const menuToggle = document.getElementById('toggle');
+    const menuItens = document.getElementById('navbarmobile');
+
+    menuToggle.classList.toggle('action-menu');
+    menuItens.classList.toggle('open-menu');
+
+}
